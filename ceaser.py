f8f5dc73821bf1003b75f96b703b68be194437de
Ivan Savinykh <ivansavinykh@mail.ru>
2022-09-26 20:09:29 +0500

    add functional

---
+++ ceaser.py
@@ -0,0 +1,18 @@
+alphabet = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+crypting_message = input("Введите зашифрованное сообщение на английском ")
+key = int(input("Введите длину ключа от 1 до 25 "))
+crypting_message = crypting_message.lower()
+crypt_message = ""
+for letter in crypting_message:
+	position = alphabet.find(letter)
+	new_position = position + key
+	if letter in alphabet:
+		crypt_message = crypt_message + alphabet[new_position]
+	else:
+		crypt_message = crypt_message + letter
+print("Ваше зашифрованное сообщение", crypt_message)
+decrypt = input("Хотите расшифровать сообщение? Да/Нет")
+if decrypt == "Да":
+	print(crypting_message.upper())
+if decrypt == "Нет":
+	print("Сообщение зашифровано")
